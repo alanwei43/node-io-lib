6c375a5825733f06bcd6e1e67bd51bdd0909c18a
alan <alan.wei@live.com>
2022-01-15 14:58:58 +0800

    refactor: 更新代码模板; 更新cli脚本扩展名判断

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -3,9 +3,11 @@
 import yargs from "yargs";
 import { getPkgInfo } from "../index";
 
+const ext = __filename.split(".")[__filename.split(".").length - 1] || "js";
+
 yargs.commandDir("commands", {
   recurse: true,
-  extensions: ["js", "ts"]
+  extensions: [ext]
 }).demandCommand()
   .version("version", "显示版本号", getPkgInfo().version)
   .help("help", "显示帮助")
