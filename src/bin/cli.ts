1522e8051dbc01e425a1c5b2ebca4ef0c4eefe53
Alan Wei <alan.wei@live.com>
2022-01-29 09:58:16 +0800

    feat: 删除多余的配置参数

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-import { getPkgInfo } from "../index";
 
 const ext = __filename.split(".")[__filename.split(".").length - 1] || "js";
 
@@ -9,6 +8,6 @@ yargs.commandDir("commands", {
   recurse: true,
   extensions: [ext]
 }).demandCommand()
-  .version("version", "显示版本号", getPkgInfo().version)
-  .help("help", "显示帮助")
+  .version()
+  .help()
   .argv;
